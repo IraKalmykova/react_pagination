6cf89a2334805cd1982b39aa78dc7ee4f958b1be
Ira <kalmykova_ira@ukr.net>
2019-08-05 18:27:43 +0300

    Updates

---
+++ precache-manifest.8788c4ea65cd325b1032479313deff10.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2681eb0a1671c6816159cc28bc0e121c",
+    "url": "/react_pagination/index.html"
+  },
+  {
+    "revision": "7b3b63fdc6b72e06e260",
+    "url": "/react_pagination/static/css/main.0a8208fd.chunk.css"
+  },
+  {
+    "revision": "ffbe32d4e790e6a5d2f2",
+    "url": "/react_pagination/static/js/2.335f0c66.chunk.js"
+  },
+  {
+    "revision": "7b3b63fdc6b72e06e260",
+    "url": "/react_pagination/static/js/main.f64541fd.chunk.js"
+  },
+  {
+    "revision": "ce8de57aa29cef69bbf2",
+    "url": "/react_pagination/static/js/runtime~main.c851b861.js"
+  }
+]);
